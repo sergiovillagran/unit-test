226509cb82c6853eb11585ae4b5751efd6afec97
Sergio Villagran <sergio.villagran@3dves.com>
2024-05-09 06:11:05 -0500

    add a real e2e test, adding the database connection also to test script in order to set seeds that let us test all the required tests, and control aty and other things

--- api/e2e/books.e2e.js
+++ api/e2e/books.e2e.js
@@ -1,36 +1,63 @@
-const mockspyGetAll = jest.fn();
 const request = require('supertest');
+const { MongoClient } = require('mongodb');
+const { config } = require('../src/config');
 const createApp = require('../src/app');
-const { generateManyBooks } = require('../src/fakes/book.fake');
 
-jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
-  getAll: mockspyGetAll,
-  create: () => { },
-})));
+const DB_NAME = config.dbName;
+const MONGO_URI = config.dbUrl;
 
 describe('Test for hello world endpoint', () => {
   let app = null;
   let server = null;
-  beforeAll(() => {
+  let database = null;
+  beforeAll(async () => {
     app = createApp();
     server = app.listen(3001);
+
+    const client = new MongoClient(
+      MONGO_URI,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    );
+
+    await client.connect();
+    database = client.db(DB_NAME);
   });
 
   afterAll(async () => {
+    await database.dropDatabase();
     await server.close();
   });
 
   describe('Test for [GET] /api/v1/books', () => {
-    test('It Should return Books array', () => {
-      const fakeBooks = generateManyBooks(20);
-      mockspyGetAll.mockResolvedValue(fakeBooks);
+    test('It Should return Books array', async () => {
+      const seedData = await database.collection('books').insertMany([
+        {
+          name: 'Book Name',
+          year: 1992,
+          author: 'Sergio Villagran',
+        },
+        {
+          name: 'Book Name',
+          year: 1992,
+          author: 'Sergio Villagran',
+        },
+        {
+          name: 'Book Name',
+          year: 1992,
+          author: 'Sergio Villagran',
+        },
+      ]);
 
       return request(app)
         .get('/api/v1/books')
         .expect(200)
         .then(({ body }) => {
           console.log(body);
-          expect(body.length).toEqual(20);
+          console.log(seedData.insertedCount);
+          expect(body.length).toEqual(seedData.insertedCount);
         });
     });
   });
---
+++ api/e2e/books.integration.js
@@ -0,0 +1,36 @@
+const mockspyGetAll = jest.fn();
+const request = require('supertest');
+const createApp = require('../src/app');
+const { generateManyBooks } = require('../src/fakes/book.fake');
+
+jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
+  getAll: mockspyGetAll,
+  create: () => { },
+})));
+
+describe('Test for hello world endpoint', () => {
+  let app = null;
+  let server = null;
+  beforeAll(() => {
+    app = createApp();
+    server = app.listen(3001);
+  });
+
+  afterAll(async () => {
+    await server.close();
+  });
+
+  describe('Test for [GET] /api/v1/books', () => {
+    test('It Should return Books array', () => {
+      const fakeBooks = generateManyBooks(20);
+      mockspyGetAll.mockResolvedValue(fakeBooks);
+
+      return request(app)
+        .get('/api/v1/books')
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.length).toEqual(20);
+        });
+    });
+  });
+});
